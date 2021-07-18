64d63ceb4ec8808345b2d79288a605441af5c73a
Maurice Yang <mauriceyangmy28@gmail.com>
2021-07-18 16:13:40 +0700

    'Debug=True'

--- ToDoWebsite/settings.py
+++ ToDoWebsite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mhdsm)ci+#vzj0gfs5zf=tp@cw#d93@2##xi_+%vfsa*^k#mxx'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shsxii-todolist.herokuapp.com', '127.0.0.1']
 
